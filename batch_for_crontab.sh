6b0885c73206966f7d8ebc8ce34176cca6df33b1
Hagen Glötter <github@dg7shg.de>
2023-02-12 13:34:35 +0100

    Update batch_for_crontab.sh

--- batch_for_crontab.sh
+++ batch_for_crontab.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Add this line to your crontab
-# */2 * * * * /usr/bin/bash batch_for_crontab.sh >/dev/null 2>&1
+# */2 * * * * /usr/bin/bash /home/pi/SEPLOS_MQTT_ioBroker/batch_for_crontab.sh >/dev/null 2>&1
 
 cd /home/pi/SEPLOS_MQTT_ioBroker || exit 
 /usr/bin/bash query_seplos_ha1.sh 4201 > BatteryPack1.txt
